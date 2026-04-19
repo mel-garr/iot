fb1aba41071771674e490da2381fe798afe9ca01
Mohamed Elg <mel-garr@student.1337.ma>
2026-04-19 02:42:01 +0100

    d

--- p3/scripts/all_in.sh
+++ p3/scripts/all_in.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-k3d cluster create --servers 1 --agents 1 -p "8888L:8888@loadbalancer"
+k3d cluster create --servers 1 --agents 1 -p "8888:8888@loadbalancer" || true
 
-kubectl create namespace argocd
-kubectl create namespace dev
+kubectl create namespace argocd || true
+kubectl create namespace dev || true
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
+kubectl wait --for=condition=available --timeout=300s deployment -n argocd --all
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 kubectl apply -f confs/
